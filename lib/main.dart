e3a1743074dbae688545f7cef25b24a819870204
PhongNgo <ptngo@akvagroup.com>
2022-05-04 01:07:30 +0700

    build new version

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/date_symbol_data_local.dart';
+// import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 import 'home_screen/home_screen.dart';
 import 'view_models/google_sheet_provider.dart';
